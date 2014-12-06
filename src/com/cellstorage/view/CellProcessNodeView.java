48f0f52f3d2e7145b407a0ff70f8ff328e91a2d1
huweiko <hwboot@163.com>
2014-12-06 22:36:38 +0800

    11

--- src/com/cellstorage/view/CellProcessNodeView.java
+++ src/com/cellstorage/view/CellProcessNodeView.java
@@ -7,6 +7,7 @@ import com.example.cellstorage.R;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -24,8 +25,13 @@ public class CellProcessNodeView extends LinearLayout{
 	private final int NODE_STATUS_DOING = 1;//正在完成的流程
 	private final int NODE_STATUS_WILLDO = 2;//还未完成的流程
 	
-	private int mNodeStatus = 0;//节点状态
+    private final int ID_IMAGENODE = 1;//节点控件的ID
 	
+    private final static int SHOW_LEFT = 1000;
+    private final static int SHOW_RIGHT = 1001;
+    
+	private int mNodeStatus = 0;//节点状态
+	private int mShowTextStatus = SHOW_RIGHT;
 	
 	public CellProcessNodeView(Context context) {
 		super(context);
@@ -38,12 +44,16 @@ public class CellProcessNodeView extends LinearLayout{
 		init(context);
 	}
 	private void init(final Context context){
+		
+		setBackgroundResource(R.color.white);
+		setOrientation(LinearLayout.VERTICAL);
+		
 		mImageViewFlow = new ImageView(context);
-		LayoutParams lpLl = new LayoutParams(
+		LinearLayout.LayoutParams lpLl = new LinearLayout.LayoutParams(
 				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		lpLl.gravity = Gravity.CENTER;
 		mImageViewFlow.setLayoutParams(lpLl);
-		mImageViewFlow.setBackgroundResource(NodeStatus[mNodeStatus]);
+		mImageViewFlow.setBackgroundResource(R.drawable.flowarrow);
 		addView(mImageViewFlow);
 		
 		RelativeLayout rlNode = new RelativeLayout(context);
@@ -51,24 +61,35 @@ public class CellProcessNodeView extends LinearLayout{
 				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 		rlNode.setLayoutParams(lpNodeParams);
 		
+
+		
+		mImageViewNode = new ImageView(context);
+		RelativeLayout.LayoutParams lpIV = new RelativeLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		lpIV.addRule(RelativeLayout.CENTER_IN_PARENT,RelativeLayout.TRUE);
+		mImageViewNode.setId(ID_IMAGENODE);
+		mImageViewNode.setLayoutParams(lpIV);
+		mImageViewNode.setBackgroundResource(NodeStatus[mNodeStatus]);
+	
 		mTextViewLeft = new TextView(context);
-		LayoutParams lpTXLeft = new LayoutParams(
+		RelativeLayout.LayoutParams lpTXLeft = new RelativeLayout.LayoutParams(
 				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		lpTXLeft.rightMargin = 10;
+		lpTXLeft.addRule(RelativeLayout.LEFT_OF,ID_IMAGENODE);
 		mTextViewLeft.setLayoutParams(lpTXLeft);
 		
 		mTextViewRight = new TextView(context);
-		LayoutParams lpTXRight = new LayoutParams(
+		RelativeLayout.LayoutParams lpTXRight = new RelativeLayout.LayoutParams(
 				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		lpTXRight.leftMargin = 10;
+		lpTXRight.addRule(RelativeLayout.RIGHT_OF,ID_IMAGENODE);
 		mTextViewRight.setLayoutParams(lpTXRight);
 		
-		mImageViewNode = new ImageView(context);
-		LayoutParams lpIV = new LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		lpIV.gravity = Gravity.CENTER;
-		mImageViewNode.setLayoutParams(lpIV);
-		
+		if(getmShowTextStatus() == SHOW_LEFT){
+			mTextViewRight.setVisibility(View.GONE);
+		}else if(getmShowTextStatus() == SHOW_RIGHT){
+			mTextViewLeft.setVisibility(View.GONE);
+		}
 		rlNode.addView(mTextViewLeft);
 		rlNode.addView(mTextViewRight);
 		rlNode.addView(mImageViewNode);
@@ -81,6 +102,18 @@ public class CellProcessNodeView extends LinearLayout{
 	}
 	public void setmNodeStatus(int mNodeStatus) {
 		this.mNodeStatus = mNodeStatus;
+	}
+	/**
+	 * @return the mShowTextStatus
+	 */
+	public int getmShowTextStatus() {
+		return mShowTextStatus;
+	}
+	/**
+	 * @param mShowTextStatus the mShowTextStatus to set
+	 */
+	public void setmShowTextStatus(int mShowTextStatus) {
+		this.mShowTextStatus = mShowTextStatus;
 	}
 	
 }
